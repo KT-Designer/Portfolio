1935267c1d9aa0aa7175005a1b98b4af9f130bbc
Doris <130676335+KT-Designer@users.noreply.github.com>
2023-07-03 09:48:06 +0800

    Add files via upload

---
+++ js/navicon.js
@@ -0,0 +1,28 @@
+// 漢堡選單
+$(document).ready(function () {
+    $('#menu-icon').click(function () {
+        $(this).toggleClass('fa-xmark');
+    });
+
+    // 點擊後，收合選單，並將ICON改成預設的樣式
+    $('.navbar a').on('click', function () {
+        $('.navbar-collapse').collapse('hide');
+        $('#menu-icon').removeClass('fa-xmark').addClass('fa-bars');
+    });
+});
+
+
+
+
+
+// 選單預設和點擊樣式
+// 在css要特別設一個.active的樣式，例如  .my-navbar .nav-link.active
+$(function () {
+    $('.navbar-nav .nav-link').click(function () {
+        // 移除之前的活動類
+        $('.navbar-nav .nav-link').removeClass('active');
+        // 添加當前點擊的菜單項的活動類
+        $(this).addClass('active');
+    });
+});
+
