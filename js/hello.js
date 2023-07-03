ce7d3befcdd47238ff4a2051a9a2777d362c5b5e
Doris <130676335+KT-Designer@users.noreply.github.com>
2023-07-03 23:49:59 +0800

    Add files via upload

---
+++ js/hello.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    function setGreeting() {
+        var greeting = $('#hello');
+        var now = new Date();
+        var hour = now.getHours();
+        var greetingText;
+
+        if (hour >= 6 && hour < 12) {
+            greetingText = 'Good morning.';
+        } else if (hour >= 12 && hour < 18) {
+            greetingText = 'Good afternoon.';
+        } else {
+            greetingText = 'Good evening.';
+        }
+
+        greeting.text(greetingText);
+    }
+
+    // 在頁面加載時設置初始問候語
+    setGreeting();
+});
