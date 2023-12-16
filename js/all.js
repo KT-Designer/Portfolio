922308a0e86c9934ab239ea710b610d6aeb98697
Doris <130676335+KT-Designer@users.noreply.github.com>
2023-12-16 23:40:59 +0800

    Add files via upload

--- js/all.js
+++ js/all.js
@@ -1,11 +1,15 @@
 (function () {
-    var disableRightClick = function (e) {
-        if (e.target.tagName === "IMG") {
-            e.preventDefault();
-            alert("別當盜圖仔！");
-        }
-    };
+    var images = document.querySelectorAll('img');
 
-    document.addEventListener('contextmenu', disableRightClick);
+    images.forEach(function (image) {
+        image.addEventListener('contextmenu', function (event) {
+            event.preventDefault();
+
+            // 將圖片更換為指定的圖片
+            image.src = '../images/bad.jpg';
+
+            // 避免觸發其他右鍵菜單
+            return false;
+        });
+    });
 })();
-
