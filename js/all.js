37366d1e935a3084a4951f9a49a39cbc3d4c0dc8
Doris <130676335+KT-Designer@users.noreply.github.com>
2024-11-02 14:12:50 +0800

    Add files via upload

--- js/all.js
+++ js/all.js
@@ -9,3 +9,24 @@
     document.addEventListener('contextmenu', disableRightClick);
 })();
 
+
+
+// 複製郵件
+function copyEmail() {
+    const emailInput = document.getElementById("emailInput");
+    emailInput.select();
+    emailInput.setSelectionRange(0, 99999); // For mobile devices
+
+    document.execCommand("copy");
+    alert("已複製 " + emailInput.value);
+}
+
+document.getElementById("copyEmailTop").addEventListener("click", copyEmail);
+document.getElementById("copyEmailBottom").addEventListener("click", copyEmail);
+
+
+
+
+
+
+
